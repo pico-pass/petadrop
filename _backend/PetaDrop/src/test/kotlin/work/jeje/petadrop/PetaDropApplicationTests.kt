05709cb3bb99b5854163719d0992434205455025
MokMinsu <mokminsu7@gmail.com>
2023-12-23 22:23:12 +0900

    [Init] #8 - Add Spring boot file

---
+++ _backend/PetaDrop/src/test/kotlin/work/jeje/petadrop/PetaDropApplicationTests.kt
@@ -0,0 +1,13 @@
+package work.jeje.petadrop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetaDropApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

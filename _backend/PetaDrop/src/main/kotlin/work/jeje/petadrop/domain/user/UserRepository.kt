4d3cf1b0277aee929a3cd61da7094835b508cd3e
jjh4450 <jjh4450git@gamil.com>
2024-01-05 02:01:48 +0900

    Changed purpose to front repo

--- _backend/PetaDrop/src/main/kotlin/work/jeje/petadrop/domain/user/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package work.jeje.petadrop.domain.user
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByEmail(email: String): Optional<User>
-
-}

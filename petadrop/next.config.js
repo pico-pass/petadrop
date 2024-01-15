e23cc1dce5f23c331f806b5fd61ab12fc6bd3150
jjh4450 <jjh4450git@gamil.com>
2024-01-16 01:46:16 +0900

    로그인 기능 구성 #6 - header footer 추가 - axios 설치 - 로그인 검증 - 쿠키 관리 추가함 TO-DO: - axios 사전 설정 전역적으로 관리하기 - 프론트-백엔드간 로그인 검증 환경 구성 완료후 리다이렉트 해주는 로직 구현하기

--- petadrop/next.config.js
+++ petadrop/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, // prevent double rendering
+    reactStrictMode: false, // prevent double rendering 
 }
 
 module.exports = nextConfig
